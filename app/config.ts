e13bda80a2bb1ff9f7b72a8e50a9f5f41d74a9c7
0xggoma <sungwoopark95@gmail.com>
2024-04-02 16:23:27 +0900

    add coinbase wallet

--- app/config.ts
+++ app/config.ts
@@ -1,12 +1,25 @@
 import { cookieStorage, createConfig, createStorage, http } from 'wagmi';
 import { degen, mainnet, sepolia } from 'wagmi/chains';
-import { injected, safe, walletConnect } from 'wagmi/connectors';
+import {
+  coinbaseWallet,
+  injected,
+  safe,
+  walletConnect,
+} from 'wagmi/connectors';
 
 const projectId = '933ceca09c35a7644b458f29423fa04c';
 
 export const config = createConfig({
   chains: [mainnet, degen, sepolia],
-  connectors: [injected(), walletConnect({ projectId }), safe()],
+  connectors: [
+    injected(),
+    coinbaseWallet({
+      appName: 'depefriend.tech',
+      appLogoUrl: 'https://depefriend.tech/android-chrome-512x512.png',
+    }),
+    walletConnect({ projectId }),
+    safe(),
+  ],
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
